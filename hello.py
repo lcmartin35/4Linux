96dda58e49a6a367dc44e75b85f24284b9382ec7
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2018-06-25 21:47:47 -0300

    aula1

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3    
+
+#Isso   é   um comentario
+
+mensagem = 'Seja bem vindo'
+nome = input('Digite  seu nome  :')
+idade = int(input('Qual sua idade ?:'))
+print('{}, {}, sua idade é {} anos'.format(mensagem,nome,idade) )
+
+
---
+++ media.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+ler o nome de um aluno
+ler duas notasss e calcular  a media
+mostrar a media do  aluno
+'''
+aluno =  input('Digite o nome : ')
+nota1 =  int(input('Digite a Nota :'))
+nota2 =  int(input('Digite a outra nota :'))
+aluno =  aluno.title() 
+# title()  deixa a primeira letra   maiscuula
+# upper()  deixa todas as letras maisculas
+# lower()  deixa todas as letras minusculas  
+# strip()  retira os espaços em branco
+# replace('a','@') altera o primeiro item pelo segundo dentro da string Ex:
+aluno = aluno.replace('i','1' )
+
+
+media = (nota1 + nota2) /2
+
+print( '{} sua media é :{:.2f} '.format(aluno,media ))      
+
+
